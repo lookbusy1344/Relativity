607240ec4b6916ec3e6886a57ea78f94e4870025
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2025-02-21 10:47:47 +0000

    Some layout changes, adding bootstrap

--- Javascript/src/main.ts
+++ Javascript/src/main.ts
@@ -10,18 +10,39 @@ let count = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('calculateButton');
-    const result = document.getElementById('result');
+    const buttonC = document.getElementById('calculateButtonC');
+    const result = document.getElementById('resultLorentz');
+    const resultC = document.getElementById('resultLightSpeed');
     const velocityInput = document.getElementById('velocityInput') as HTMLInputElement;
+    const velocityInputC = document.getElementById('velocityInputC') as HTMLInputElement;
 
     if (button && result && velocityInput) {
         button.addEventListener('click', () => {
             try {
                 const v = rl.checkVelocity(velocityInput.value ?? 0);
                 const l = rl.lorentzFactor(v);
-                result.textContent = `${v} m/s, lorentz factor: ${l.toPrecision(20)} (clicks: ${++count})`;
+                result.textContent = l.toPrecision(20); // (clicks: ${++count})`;
             } catch (err) {
                 const error = err as Error;
-                result.textContent = `Error: ${error.message}`;
+                result.textContent = error.message;
+            }
+        });
+    }
+
+    if (buttonC && resultC && velocityInputC) {
+        buttonC.addEventListener('click', () => {
+            try {
+                const v = rl.ensure(velocityInputC.value ?? 0);
+                const fraction = new Decimal(v).dividedBy(rl.c);
+                resultC.textContent = `${fraction.toPrecision(30)}c`;
+                if (fraction.greaterThan(1)) {
+                    resultC.style.color = 'red';
+                } else {
+                    resultC.style.color = 'black';
+                }
+            } catch (err) {
+                const error = err as Error;
+                resultC.textContent = error.message;
             }
         });
     }
