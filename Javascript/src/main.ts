3def2e2924eb3bf9c8871d14dae6dbd91b9e7fd2
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2025-03-03 11:47:14 +0000

    Removing exception catching, since they are no longer thrown

--- Javascript/src/main.ts
+++ Javascript/src/main.ts
@@ -49,102 +49,65 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (lorentzButton && resultLorentz && lorentzInput) {
         lorentzButton.addEventListener('click', () => {
-            try {
-                const vel = rl.checkVelocity(lorentzInput.value ?? 0);
-                const lorentz = rl.lorentzFactor(vel);
-                setElement(resultLorentz, rl.formatSignificant(lorentz, "0", 3), "");
-            } catch (err) {
-                const error = err as Error;
-                resultLorentz.textContent = error.message;
-            }
+            const vel = rl.checkVelocity(lorentzInput.value ?? 0);
+            const lorentz = rl.lorentzFactor(vel);
+            setElement(resultLorentz, rl.formatSignificant(lorentz, "0", 3), "");
         });
     }
 
     if (velocityButton && resultVelocity && velocityInput) {
         velocityButton.addEventListener('click', () => {
-            try {
-                const rapidity = rl.rapidityFromVelocity(velocityInput.value ?? 0);
-                setElement(resultVelocity, rl.formatSignificant(rapidity, "0", 3), "");
-            } catch (err) {
-                const error = err as Error;
-                resultVelocity.textContent = error.message;
-            }
+            const rapidity = rl.rapidityFromVelocity(velocityInput.value ?? 0);
+            setElement(resultVelocity, rl.formatSignificant(rapidity, "0", 3), "");
         });
     }
 
     if (rapidityButton && resultRapidity && rapidityInput) {
         rapidityButton.addEventListener('click', () => {
-            try {
-                const velocity = rl.velocityFromRapidity(rapidityInput.value ?? 0);
-                setElement(resultRapidity, rl.formatSignificant(velocity, "9", 3), "m/s");
-            } catch (err) {
-                const error = err as Error;
-                resultRapidity.textContent = error.message;
-            }
+            const velocity = rl.velocityFromRapidity(rapidityInput.value ?? 0);
+            setElement(resultRapidity, rl.formatSignificant(velocity, "9", 3), "m/s");
         });
     }
 
     // relativistic velocity and distance
     if (aButton && resultA && resultA1 && resultA2 && aInput) {
         aButton.addEventListener('click', () => {
-            try {
-                const accel = rl.g; // just assume 1g
-                const secs = rl.ensure(aInput.value ?? 0).mul(60 * 60 * 24); // days to seconds
+            const accel = rl.g; // just assume 1g
+            const secs = rl.ensure(aInput.value ?? 0).mul(60 * 60 * 24); // days to seconds
 
-                const relVel = rl.relativisticVelocity(accel, secs);
-                const relDist = rl.relativisticDistance(accel, secs);
-                // const simpleVel = accel.mul(secs);
-                // const simpleDist = rl.simpleDistance(accel, secs);
-                const relVelC = relVel.div(rl.c);
-                const relDistC = relDist.div(rl.lightYear);
+            const relVel = rl.relativisticVelocity(accel, secs);
+            const relDist = rl.relativisticDistance(accel, secs);
+            const relVelC = relVel.div(rl.c);
+            const relDistC = relDist.div(rl.lightYear);
 
-                setElement(resultA1, rl.formatSignificant(relVel, "9", 3), "m/s");
-                setElement(resultA2, relDist.toPrecision(20), "m");
+            setElement(resultA1, rl.formatSignificant(relVel, "9", 3), "m/s");
+            setElement(resultA2, relDist.toPrecision(20), "m");
 
-                setElement(resultA1b!, rl.formatSignificant(relVelC, "9", 3), "c");
-                setElement(resultA2b!, rl.formatSignificant(relDistC, "0", 3), "ly");
-                resultA.textContent = "";
-            } catch (err) {
-                const error = err as Error;
-                resultA.textContent = error.message;
-                setElement(resultA1, "-", "");
-                setElement(resultA2, "-", "");
-                setElement(resultA1b!, "-", "");
-                setElement(resultA2b!, "-", "");
-            }
+            setElement(resultA1b!, rl.formatSignificant(relVelC, "9", 3), "c");
+            setElement(resultA2b!, rl.formatSignificant(relDistC, "0", 3), "ly");
+            resultA.textContent = "";
         });
     }
 
     // flip and burn
     if (flipButton && resultFlip1 && resultFlip2 && resultFlip3 && resultFlip4 && flipInput) {
         flipButton.addEventListener('click', () => {
-            try {
-                const m = rl.ensure(flipInput.value ?? 0).mul(rl.lightYear); // convert light years to meters
-                let res = rl.flipAndBurn(rl.g, m);
-                const properTime = res.properTime.div(rl.secondsPerYear); // convert to years
-                const coordTime = res.coordTime.div(rl.secondsPerYear); // convert to years
-                const peak = res.peakVelocity.div(rl.c); // convert to fraction of c
-                const lorentz = res.lorentzFactor;
-                const metre = rl.formatSignificant(rl.one.div(lorentz), "0", 2);
-                const sec = rl.formatSignificant(rl.one.mul(lorentz), "0", 2);
+            const m = rl.ensure(flipInput.value ?? 0).mul(rl.lightYear); // convert light years to meters
+            let res = rl.flipAndBurn(rl.g, m);
+            const properTime = res.properTime.div(rl.secondsPerYear); // convert to years
+            const coordTime = res.coordTime.div(rl.secondsPerYear); // convert to years
+            const peak = res.peakVelocity.div(rl.c); // convert to fraction of c
+            const lorentz = res.lorentzFactor;
+            const metre = rl.formatSignificant(rl.one.div(lorentz), "0", 2);
+            const sec = rl.formatSignificant(rl.one.mul(lorentz), "0", 2);
 
-                setElement(resultFlip1, rl.formatSignificant(properTime, "0", 2), "yrs");
-                setElement(resultFlip2, rl.formatSignificant(peak, "9", 2), "c");
-                setElement(resultFlip4, rl.formatSignificant(coordTime, "0", 2), "yrs");
+            setElement(resultFlip1, rl.formatSignificant(properTime, "0", 2), "yrs");
+            setElement(resultFlip2, rl.formatSignificant(peak, "9", 2), "c");
+            setElement(resultFlip4, rl.formatSignificant(coordTime, "0", 2), "yrs");
 
-                setElement(resultFlip3, rl.formatSignificant(lorentz, "0", 2), "");
-                setElement(resultFlip5!, `1m becomes ${metre}m`, "");
-                setElement(resultFlip6!, `1s becomes ${sec}s`, "");
-            } catch (err) {
-                const error = err as Error;
-                resultFlip1.textContent = error.message;
-                setElement(resultFlip2, "-", "");
-                setElement(resultFlip3, "-", "");
-                setElement(resultFlip4, "-", "");
-                setElement(resultFlip5!, "-", "");
-                setElement(resultFlip6!, "-", "");
-            }
+            setElement(resultFlip3, rl.formatSignificant(lorentz, "0", 2), "");
+            setElement(resultFlip5!, `1m becomes ${metre}m`, "");
+            setElement(resultFlip6!, `1s becomes ${sec}s`, "");
         });
     }
-
 });
