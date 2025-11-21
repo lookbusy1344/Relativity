1bafbab077f1b0cdd22743425cce96ea543750a7
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2025-11-21 10:07:19 +0000

    WebStorm config for debugging in Chrome (doesn't work in Safari or Edge)

--- Javascript/vite.config.ts
+++ Javascript/vite.config.ts
@@ -2,4 +2,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './', // Use relative paths instead of absolute paths from root
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    sourcemapIgnoreList: false,
+  },
 });
